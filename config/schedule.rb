be518d3d890a54f6419302d0f74f281249be631d
Sebastián Katzer <katzer.sebastian@googlemail.com>
2016-05-31 10:05:57 +0200

    Removed environment_variable placeholder

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,7 +5,8 @@
 
 set :path, ENV.fetch('APP_HOME', Whenever.path)
 set :job_template, "/bin/sh -l -c ':job'"
-set :environment_variable, 'APP_ENV'
+
+job_type :rake, 'cd :path && :bundle_command rake :task --silent :output'
 
 every :day, at: '11pm' do
   rake 'fetch:stocks drive:upload'
